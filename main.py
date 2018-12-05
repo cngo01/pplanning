5e625776c9db587cb990145f573503b2a8c3fde1
cngo01 <44450899+cngo01@users.noreply.github.com>
2018-12-05 13:59:47 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -5,5 +5,83 @@ Created on Fri Nov 16 11:52:54 2018
 
 @author: cngo
 """
+from math import sqrt
+import pathv1 #Astar
+import matplotlib.pyplot as plt
+import Dynamic_Window
 
+def pathlength(x,y):
+    n = len(x) 
+    lv = [sqrt((x[i]-x[i-1])**2 + (y[i]-y[i-1])**2) for i in range (1,n)]
+    L = sum(lv)
+    return L
+#%%
+ob = np.matrix([[0, 2],
+                [2.5, 8.0],
+                [4.0, 2.0],
+                [5.0, 4.0],
+                [5.0, 5.0],
+                [5.0, 6.0],
+                [5.0, 9.0],
+                [8.0, 9.0],
+                [7.0, 9.0],
+                [7.0, 6.2],
+                [12.0, 12.0],
+                [12.0, 14.0],
+                [13.0, 13.0]
+                ])
+Ps=([90.17962699, 18.89381439, 20.28729583, 93.90378764, 63.28318137,
+       85.05038426, 29.05149061, 38.55092059, 87.28532153, 19.05553487,
+       20.01487473, 40.46982651, 59.25035024])
+    
+to_goal_cost_gain=1
+robot_radius=2
+finalPath=pathv1.main(to_goal_cost_gain,robot_radius)
+Astar=np.array(finalPath)
+
+x=Astar[:,0]
+y=Astar[:,1]
+L_star=pathlength(x,y)
+print(L_star)
+
+#plt.plot(traj[:, 0], traj[:, 1], "-r")
+ob_a=np.array(ob)
+for i in range(len(ob)):
+    xb = ob_a[i,0]
+    yb = ob_a[i,1]
+    plt.plot(xb, yb, 'bo')
+    plt.text(xb * (1 + 0.01), yb * (1 + 0.01),np.round(Ps[i],2), fontsize=10)
+plt.plot(x,y,"-r")
+plt.grid(True)
+plt.plot(0, 0, "xr")
+plt.plot(goal[0], goal[1], "xb")
+plt.show()
+
+#%% Dynamic Window
+
+
+#x = traj[:,0]#[pts[i][0] for i in range(n)]
+#y = traj[:,1]#[pts[i][1] for i in range(n)]
+
+# Now call the function with the x and y 
+# and print the result
+#l = pathlength(x, y)
+#print(l)
 #generate probability map
+
+#Test different MDP alg
+
+#Dijstar
+#A*
+#MC
+
+#POMDP
+#Online Particle
+#MC
+
+#Animation
+
+#Compare
+#traj = [x,y, u, dt]
+
+#Astar_backup=Astar #when cost of 5 and 40
--- pathv1.py
+++ pathv1.py
@@ -48,8 +48,32 @@ class Node:
 
     def getParentNode(self):
         return self.parent
+def calc_obstacle_cost(x,y, ob,robot_radius,Ps):
+    #calc_obstacle_cost(neighborNode.getx(),neighborNode.gety(), ob,robot_radius,Ps)
+    # calc obstacle cost inf: collistion, 0:free
+    minr = float("inf")
+    #print(x,y)
+    for i in range(len(ob[:, 0])):
+        ox = ob[i, 0]
+        oy = ob[i, 1]
+        dx = x - ox
+        dy = y - oy
+        
+        r = math.sqrt(dx**2 + dy**2)
+        if r == 0:
+            
+            return 10000
+        elif r <= robot_radius:
+            r_neg=Ps[i]
+            #print(r_neg)
+            return r_neg#float("Inf")  # collisiton
+        
+        if minr >= r:
+            minr = r
+   
+    return 1.0 / minr  # OK
 
-def AStar(nSx,nSy,nTx,nTy):
+def AStar(nSx,nSy,nTx,nTy,to_goal_cost_gain,robot_radius):
     print('here')
     # CREATE RISK MAP
     #map = np.zeros((2,2))
@@ -67,18 +91,21 @@ def AStar(nSx,nSy,nTx,nTy):
                 [12.0, 14.0],
                 [13.0, 13.0]
                 ])
-    map = np.zeros([15,15])
-    Ps=np.random.rand(15)
+    gmap = np.zeros([15,15])
+    Ps=([90.17962699, 18.89381439, 20.28729583, 93.90378764, 63.28318137,
+           85.05038426, 29.05149061, 38.55092059, 87.28532153, 19.05553487,
+           20.01487473, 40.46982651, 59.25035024])
+    
     for i in range(len(ob)):
-        map[i,i]=Ps[i]
+        gmap[i,i]=Ps[i]
     print('check here')
-    print(map)
+    print(gmap)
 
     #map = np.array([[0.9, 0, 0],[0, 0.9, 0],[0, 0.3, 0.1]])
     #size_map_x,size_map_y = map.shape()
-    cost_map = map*100
-    print(map)
-    xdim, ydim = np.shape(map)
+    cost_map = gmap*100
+    print(gmap)
+    xdim, ydim = np.shape(gmap)
     print('xdim:',xdim)
     print('ydim:',ydim)
     nTx = xdim-1 # 0-based index
@@ -138,14 +165,16 @@ def AStar(nSx,nSy,nTx,nTy):
         if (current_x, current_y) == (nTx, nTy):
             finalPath = reconstructPath(currentNode,nSx,nSy)
             break
-        neighbors_list = getNeighbors(current_x,current_y,xdim,ydim) # return (node, cost of motion)
+        neighbors_list = getNeighbors(current_x,current_y,xdim,ydim,nTx,nTy,to_goal_cost_gain) # return (node, cost of motion)
         #print(neighbors_list)
         innercount = 0
         for ( nID, neighborNode, cost_motion) in neighbors_list:
             #print('innercount',innercount)
             innercount += 1
             if neighborNode.checkVisited() == False:
-                new_cost = currentNode_cost + cost_motion + cost_map[neighborNode.getx()][neighborNode.gety()]
+                obstacle_cost=calc_obstacle_cost(neighborNode.getx(),neighborNode.gety(), ob,robot_radius,Ps)
+                new_cost = currentNode_cost + cost_motion + obstacle_cost#cost_map[neighborNode.getx()][neighborNode.gety()]
+                #new_cost = currentNode_cost + cost_motion + cost_map[neighborNode.getx()][neighborNode.gety()]
                 if neighborNode.getCost() > new_cost:
                     neighborNode.addPrev(current_x, current_y)
                     neighborNode.addParentNode(currentNode)
@@ -162,8 +191,18 @@ def AStar(nSx,nSy,nTx,nTy):
                     neighborNode.setCost(new_cost)
     print('finalPath')
     print(finalPath)
+    return finalPath
+def calc_to_goal_cost(x,y,nTarget_x,nTarget_y, to_goal_cost_gain):
+    # calc to goal cost. It is 2D norm.
 
-def getNeighbors(cur_x ,cur_y, xsize, ysize):
+    dy = nTarget_x - x
+    dx = nTarget_y - y
+    goal_dis = math.sqrt(dx**2 + dy**2)
+    cost = to_goal_cost_gain * goal_dis
+
+    return cost
+
+def getNeighbors(cur_x ,cur_y, xsize, ysize,nTarget_x,nTarget_y,to_goal_cost_gain):
     neighbor_list = [(x,y) for x in range(cur_x-1,cur_x+2) for y in range(cur_y-1,cur_y+2) if (x,y) != (cur_x,cur_y) and 0<=x<xsize and 0<=y<ysize]
     #print(neighbor_list)
     final_list = []
@@ -172,10 +211,12 @@ def getNeighbors(cur_x ,cur_y, xsize, ysize):
         if nodeID in global_variables['vistitedID_set']:
             continue
         #print (x,y)
-        if x != cur_x and y != cur_y:
-            cost = 5
-        else:
-            cost = 40
+#        if x != cur_x and y != cur_y:
+#            cost = 5
+#        else:
+#            cost = 40
+        cost=calc_to_goal_cost(x,y,nTarget_x,nTarget_y,to_goal_cost_gain)
+        #print(cost)
         # NEW RETURN: RETURN (NODE,NODEID,COST_MOTION)
         final_list.append(( nodeID, Node(x,y,nodeID) , cost ))
         #final_list.append((x,y,getID(x,y,ysize),cost))
@@ -200,11 +241,13 @@ def reconstructPath(curNode,startX,startY):
 def getID(x,y,ysize):
     return x*ysize + y
 
-def main():
-    if len(sys.argv) != 3:
-        raise Exception("usage: python pathv1.py n_grid_x n_grid_y")
-    gridsize_x = int(sys.argv[1])
-    gridsize_y = int(sys.argv[2])
+def main(to_goal_cost_gain,robot_radius):
+#    if len(sys.argv) != 3:
+#        raise Exception("usage: python pathv1.py n_grid_x n_grid_y")
+#    gridsize_x = int(sys.argv[1])
+#    gridsize_y = int(sys.argv[2])
+    gridsize_x=15
+    gridsize_y=15
     #print(gridsize_x)
     #print(type(gridsize_x))
     #print(gridsize_y)
@@ -213,7 +256,8 @@ def main():
     nStart_y = 0
     nTarget_x = gridsize_x-1
     nTarget_y = gridsize_y-1
-    AStar(nStart_x,nStart_y,nTarget_x,nTarget_y)
+
+    finalPath=AStar(nStart_x,nStart_y,nTarget_x,nTarget_y,to_goal_cost_gain,robot_radius)
     ## import drone_env.py to create gridworld
 
     ## TEST CREATENODE CLASS
@@ -231,8 +275,8 @@ def main():
     #Node.addCost(x,10)
     #print(x)
     #print(Node.getPrev(x))
-
+    return finalPath
 
 
 if __name__ == '__main__':
-    main()
+    finalPath=main(to_goal_cost_gain,robot_radius)
